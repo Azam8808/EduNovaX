7defb619c0d3b151f83c17ab3d6bc615d29e52ac
Azam <azamkhan32451@gmail.com>
2026-03-27 21:54:18 +0530

    upated code

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("DB Connected Successfully");
+    } catch (error) {
+        console.log("DB Connection Failed");
+        console.error(error);
+        process.exit(1);
+    }
+};
