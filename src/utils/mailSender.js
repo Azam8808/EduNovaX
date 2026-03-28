a94f38f114bcd112e8ee668a313d27a74816fbfa
Azam <azamkhan32451@gmail.com>
2026-03-28 14:38:47 +0530

    updated code

--- src/utils/mailSender.js
+++ src/utils/mailSender.js
@@ -3,24 +3,24 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
     try{
             let transporter = nodemailer.createTransport({
-                
-                host:process.env.MAIL_HOST,
+                host: process.env.MAIL_HOST,
+                port: 587,
+                secure: false,
                 auth:{
                     user: process.env.MAIL_USER,
                     pass: process.env.MAIL_PASS,
                 }
-            })
-
+            });
 
             let info = await transporter.sendMail({
-                from: 'TechStudy-Hub || CodeHelp - by Babbar',
+                from: 'EduNovaX',
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
             })
             console.log(info);
             return info;
-    }
+        }
     catch(error) {
         console.log(error.message);
     }
